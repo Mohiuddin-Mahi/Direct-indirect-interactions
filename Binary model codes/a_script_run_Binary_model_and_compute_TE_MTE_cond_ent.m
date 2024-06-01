6dffcd381a71e8b17c27659d908e6dfbf574fb10
Mohiuddin-Mahi <98726845+Mohiuddin-Mahi@users.noreply.github.com>
2024-06-01 18:38:45 +0900

    Add files via upload

---
+++ Binary model codes/a_script_run_Binary_model_and_compute_TE_MTE_cond_ent.m
@@ -0,0 +1,16 @@
+
+clc
+close all
+clear;
+
+%%%% Generate data %%%%
+
+run_Binary_model_to_generate_data
+
+%%%% Computation of TE and modified TE %%%%
+
+function_TE_modified_TE_computation
+
+%%%%% Conditional entropy computation %%%
+
+function_conditional_entropy_computation
