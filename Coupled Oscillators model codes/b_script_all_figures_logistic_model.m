fc02ddbbc58bbe1f2e0d03a96b6a879345d812cf
Mohiuddin-Mahi <98726845+Mohiuddin-Mahi@users.noreply.github.com>
2024-09-24 15:53:24 +0900

    Update b_script_all_figures_logistic_model.m

--- Coupled Oscillators model codes/b_script_all_figures_logistic_model.m
+++ Coupled Oscillators model codes/b_script_all_figures_logistic_model.m
@@ -3,7 +3,7 @@ clc
 close all
 clear;
 
-font=20;
+font=23;
 tau_max=4;
 num_trail=20;
 nsymbols=16;
@@ -17,14 +17,12 @@ chaos_ind=11;
 
 function_te_mte_logistic_figure(font,chaos_ind,coup_min,coup_max,num_coup,tau_max)
 
-%%%%%%%%%%%% Cond_ent strength upto 1 %%%%%%%%
+%%%%%%%%%%%% Cond_ent strength upto 1 and timeseries %%%%%%%%
 coup_max1=1;
 num_coup1=21;
 chaos_ind1=21;
-function_cond_ent_figure_logistic(font,chaos_ind1,coup_min,coup_max1,num_coup1)
+function_cond_ent_timseries_figure_logistic(font,chaos_ind1,coup_min,coup_max1,num_coup1)
 
-%%%%%%%%%%%%% Synchronized and non synchronized time series %%%%%%%%
 
-function_logistic_timeseries_figure(font)
 
 
