e113c4ba8ae5efedd6f5425b41203bceca3981ff
Mohiuddin-Mahi <98726845+Mohiuddin-Mahi@users.noreply.github.com>
2024-09-24 15:35:18 +0900

    Update b_script_all_figures.m

--- Binary model codes/b_script_all_figures.m
+++ Binary model codes/b_script_all_figures.m
@@ -3,16 +3,8 @@ clc
 close all
 clear;
 
-font=20;
+font=23;
 
-%%%% TE Figures %%%%
+%%%% TE & cond ent & heatmap Figures %%%%
 
-Figures_TE_modified_TE_Binary_model(font)
-
-%%%% Heatmap %%%%
-
-function_heatmap_Binary_model(font)
-
-%%%%% Conditional entropy Figures %%%
-
-Figures_cond_ent_Binary_model(font)
+Figures_TE_cond_ent_heatmap_Binary_model(font)
